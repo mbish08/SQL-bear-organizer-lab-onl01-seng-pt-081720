7d7e9f77a4c85bba70cd7fc92ba7c821caccf239
Mikel Bishop <mdrew8@gmail.com>
2020-10-24 01:21:12 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,2 +1,23 @@
 INSERT INTO bears (name, age, gender, color, temperament, alive)
 VALUES ("Mr.Chocolate", 1, female, brown, mellow, true);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Rowdy", 2, male, white, rowdy, true);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Tabitha", 3, female, black, fun, true);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Sergeant Brown", 4, male, red, bossy, true);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Melissa", 5, female, green, flaky, true);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Grinch", 5, male, green, happy, true);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Wendy", 6, female, purple, lonely, true);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES (null, 7, male, orange, cheesy, false);
